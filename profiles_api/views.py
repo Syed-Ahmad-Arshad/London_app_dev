a8d084a65ad3bf370f6ff37924ab55ea61de9337
syed-ahmad-arshad <bsef16m009@pucit.edu.pk>
2021-08-04 10:41:49 +0500

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,7 +7,8 @@ from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 from rest_framework import filters
-
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 class HelloAPIView(APIView):
     """ Test API View """
     serializer_class = serializers.HelloSerializer
@@ -73,3 +74,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class USerLoginAPIView(ObtainAuthToken):
+    """ Handle creating user authentication tokens """
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    
