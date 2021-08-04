d7a3df1baead6a01d78fa8e9b3410ec7d30b8185
syed-ahmad-arshad <bsef16m009@pucit.edu.pk>
2021-08-04 10:20:37 +0500

    Added profile viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import  Response
 from rest_framework import status
-from profiles_api import serializers
+from profiles_api import permissions, serializers
+from rest_framework import viewsets
+from profiles_api import models
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
 
 
 class HelloAPIView(APIView):
@@ -42,3 +46,28 @@ class HelloAPIView(APIView):
         """ Delete an object """
         return Response({'method':'DELETE'})
 
+class HelloViewSet(viewsets.ViewSet):
+    """ Test API Viewset"""
+    serializer_class = serializers.HelloSerializer
+    def list(self, request):
+        """ Returna a hello message"""
+        a_viewset = [ 
+            'Uses actions (list, create, retrieve, update, partial_update',
+            'Autmoatically maps to URLs using Routers',
+            'Provides more functioanlity with more code',
+
+            ]
+        return Response({'message':'Hello!', 'a_viewset':a_viewset})
+
+
+    def create(self, request):
+        """ Create a new hello message """
+        serializer = self.serializer_class(data=request.data)
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """ Handle creating and updating profiles """
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all() 
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (permissions.UpdateOwnProfile,)
